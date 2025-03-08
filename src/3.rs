8cfa88ad1d6b28fde00a53bd6b8edeba71c4d4b9
rueterlaughlin457 <rueterlaughlin457@gmail.com>
2025-03-08 20:14:51 +0000

    Add 3.rs

---
+++ src/3.rs
@@ -0,0 +1,8 @@
+
+// This is a Rust program that takes two numbers as input and returns their sum.
+fn main() {
+    let num1 = 5;
+    let num2 = 8;
+    
+    println!("The sum of {} and {} is {}", num1, num2, num1 + num2);
+}
