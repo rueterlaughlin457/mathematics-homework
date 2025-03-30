f351f628d4dd79d50c3924ef1ebfe2d576adf78b
rueterlaughlin457 <rueterlaughlin457@gmail.com>
2025-03-30 14:52:16 +0000

    Add 21.rs

---
+++ src/21.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // Example usage of Rust functions and structures
+    let number = 10;
+    let squared_number = number * number; // Squaring the number
+    println!("The square of {} is {}", number, squared_number);
+}
